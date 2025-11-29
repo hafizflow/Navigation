50adeda83e78121a5ee89bafdeea38065e782a29
Hafizur Rahman <hafizur.rahman.cs@gmail.com>
2025-11-29 09:18:23 +0600

    Initial Commit

---
+++ Navigation/NavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationApp.swift
+//  Navigation
+//
+//  Created by Hafizur Rahman on 29/11/25.
+//
+
+import SwiftUI
+
+@main
+struct NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
