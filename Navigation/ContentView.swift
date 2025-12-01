03057ca302a8fa6ae77f5a4b295ab644d606c2d1
Hafizur Rahman <hafizur.rahman.cs@gmail.com>
2025-12-01 23:44:21 +0600

    Complete

--- Navigation/ContentView.swift
+++ Navigation/ContentView.swift
@@ -10,6 +10,10 @@ struct DetailView: View {
     var body: some View {
         NavigationLink("Go to next page", value: Int.random(in: 1...1000))
             .navigationTitle("Page \(num)")
+            .navigationBarTitleDisplayMode(.automatic)
+            .toolbar {
+                Button("Hello", systemImage: "action") {}
+            }
     }
 }
 
